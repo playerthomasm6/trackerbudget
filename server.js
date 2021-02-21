9a8c8e2f259839db75122f1cd20b351877509547
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2021-02-21 15:37:33 -0500

    added html routes

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ mongoose.connect(
 
 // routes
 app.use(require("./routes/api.js"));
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
