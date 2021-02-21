057f805f56264083cf52131924cbd6c143b59789
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2021-02-21 16:14:39 -0500

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,8 +3,8 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     '/',
     'index.html',
-    '/assets/css/style.css',
-    ''
+    'styles.css',
+    "/manifest.webmanifest",
 ];
 
 
